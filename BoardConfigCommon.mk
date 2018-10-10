7e4f6156884a2a8ce50239d8388891a2e2ec1f8f
Rick M <spookcity138@gmail.com>
2018-10-10 10:25:22 +0200

    Set SDK API level for required executables
    This singularly handles both text relocation (M) and mutex (P) linker
    restrictions
    
    * mediaserver - Needs text relocations

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,9 @@ TARGET_SOC := exynos3470
 TARGET_LD_SHIM_LIBS := /system/vendor/lib/hw/camera.vendor.universal3470.so|libshim_camera.so
 
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
-    /system/vendor/bin/hw/rild=27
+    /system/vendor/bin/hw/rild=27 \
+    /system/bin/mediaserver=22
+
 # OpenMAX-shims
 TARGET_LD_SHIM_LIBS += \
 	/system/lib/omx/libOMX.Exynos.AVC.Decoder.so|/vendor/lib/libui_shim.so \

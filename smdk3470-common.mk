e7b56d0522f002430c45c2e2b1ba6e7329b46639
Rick M <spookcity138@gmail.com>
2018-10-25 10:36:40 +0200

    Deprecate SamsungServiceMode

--- smdk3470-common.mk
+++ smdk3470-common.mk
@@ -60,10 +60,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
-# Samsung
-PRODUCT_PACKAGES += \
-    SamsungServiceMode
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.universal3470 \

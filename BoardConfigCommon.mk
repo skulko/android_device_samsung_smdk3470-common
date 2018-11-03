bea98ef37c67eeaafbb8d57e92c6a9933787aeaa
Rick M <spookcity138@gmail.com>
2018-11-03 10:04:35 +0100

    Camera:Allow additional gralloc 1.0 buffer usage bits
    Our camera requires 0x4000000. This seems to get the camera mostly
    working (photos)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,6 +95,7 @@ BOARD_CUSTOM_BT_CONFIG := device/samsung/smdk3470-common/bluetooth/vnd_smdk3470.
 # Graphics
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 USE_OPENGL_RENDERER := true
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x4000000
 BOARD_EGL_CFG := device/samsung/smdk3470-common/egl/egl.cfg
 
 # Camera

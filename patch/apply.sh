a5fed90dc343c3a6bbe6b4cb0d0c30f2a99bbeed
Rick M <spookcity138@gmail.com>
2018-12-09 21:24:49 +0100

    Patch:Update apply.sh
    Remove some unneeded bits.

--- patch/apply.sh
+++ patch/apply.sh
@@ -4,13 +4,6 @@ MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
-cd "$CMBASE/bootable/recovery"
-if git remote -v | grep omnirom; then
-    TWRP=true
-else
-    TWRP=false
-fi
-
 for i in $(find "$PATCHBASE"/* -type d); do
     PATCHNAME=$(basename "$i")
     PATCHTARGET=$PATCHNAME
@@ -24,8 +17,6 @@ for i in $(find "$PATCHBASE"/* -type d); do
     cd "$CMBASE/$PATCHTARGET" || exit 1
 
     suffix=".patch"
-    if [ $PATCHNAME = "bootable_recovery" -a $TWRP = true ]; then suffix=".twrp.patch"; fi
-    if [ $PATCHNAME = "bootable_recovery" -a $TWRP = false ]; then suffix=".cm.patch"; fi
 
     if compgen -G "$PATCHBASE/$PATCHNAME/*${suffix}" > /dev/null; then
         if ! git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*${suffix}; then

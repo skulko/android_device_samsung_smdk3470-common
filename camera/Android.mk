1a759a2c52d8563939710591132981904f25a55d
skulko <23030592+skulko@users.noreply.github.com>
2020-05-24 11:23:40 +0200

    camera: update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -6,16 +6,16 @@ PROPRIETARY_PATH := ../../../../vendor/samsung/$(TARGET_DEVICE)/proprietary
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_STATIC_LIBRARIES := libbase libarect
+LOCAL_STATIC_LIBRARIES := libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    libhardware liblog libcamera_client libgui libui \
+    libhidltransport libutils android.hidl.token@1.0-utils
 
 LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
@@ -38,4 +38,3 @@ LOCAL_MODULE_TAGS	:= optional
 LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_PREBUILT)
-

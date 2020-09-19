8cd5bc72399f0f8f4b43e560bc1c5ba4e48a1593
skulko <23030592+skulko@users.noreply.github.com>
2020-09-19 11:43:33 +0200

    Remvove gnss-service

--- smdk3470-common.mk
+++ smdk3470-common.mk
@@ -167,8 +167,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl
 
 # Keymaster
 PRODUCT_PACKAGES += \
